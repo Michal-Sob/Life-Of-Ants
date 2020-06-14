cc6c997e09a002906fddb7a3fd3569236bd25cbd
Michal <michalkacpernowak@gmail.com>
2020-06-14 19:44:06 +0200

    Start

---
+++ Map.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LifeOfAnts
+{
+	public class Map
+	{
+		public int[,] board;
+
+		Map(int nRows, int nCols)
+		{
+			board = new int[nRows, nCols];
+		}
+
+	}
+}
---
+++ Queen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LifeOfAnts
+{
+	public class Queen : Ant
+	{
+        private static volatile Queen SoleInstance = new Queen();
+
+        private Queen()
+			:base()
+		{
+
+		}
+
+        public static Queen GetInstance()
+		{
+            return SoleInstance;
+		}
+	}
+}
